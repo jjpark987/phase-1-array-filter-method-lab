cf9393ae243eb624a96bcfb6174618226855b562
Jason Park <jjpark987@gmail.com>
2022-10-25 13:36:46 -0700

    commit tree 2555931bf3233e3cec01e377483af5ef98249be1

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Code your solution here
+function findMatching(listOfNames, name) {
+//    const newListOfNames = [];
+
+//     for(const element of listOfNames) {
+//         if(element.toLowerCase() === name.toLowerCase()) {
+//             newListOfNames.push(element);
+//         }
+//     }
+
+//     return newListOfNames;
+   
+    const newListOfNames = listOfNames.filter(function(element) {
+        return element.toLowerCase() === name.toLowerCase();
+    });
+    
+    return newListOfNames;
+}
+
+function fuzzyMatch(listOfNames, name) {
+    const newListOfNames = listOfNames.filter(function(element) {
+        return element[0] === name[0];
+    });
+
+    return newListOfNames;
+}
+
+function matchName(driversInfo, matchingString) {
+    const list = driversInfo.filter(function(element) {
+        return element.name === matchingString;
+    });
+
+    return list;
+}
